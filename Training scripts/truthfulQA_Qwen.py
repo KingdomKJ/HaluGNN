d3ec1039b583dd7edb3442f38db6e04bbca84378
CullenDs <40348774+KingdomKJ@users.noreply.github.com>
2025-12-05 23:08:03 +0800

    Update truthfulQA_Qwen.py

--- Training scripts/truthfulQA_Qwen.py
+++ Training scripts/truthfulQA_Qwen.py
@@ -36,22 +36,17 @@ class GNNModel(torch.nn.Module):
         return x
 
 
-train_file_name = f'TruthfulQA/Qwen/{args.type}/TruthfulQA_Qwen_train_data_list.pkl'
-test_file_name = f'TruthfulQA/Qwen/{args.type}/TruthfulQA_Qwen_test_data_list.pkl'
-
-with open(train_file_name, 'rb') as file1:
-    train_data = pickle.load(file1) 
-
-with open(test_file_name, 'rb') as file2:
-    test_data = pickle.load(file2)
+train_file_name = f'truthfulQA/Qwen/{args.type}/truthfulQA_Qwen_train_data_list.pkl'
+validation_file_name = f'truthfulQA/Qwen/{args.type}/truthfulQA_Qwen_validation_data_list.pkl'
+test_file_name = f'truthfulQA/Qwen/{args.type}/truthfulQA_Qwen_test_data_list.pkl'
 
 
 train_data_list = train_data
+validation_data_list=validation_data
 test_data_list = test_data
-
-train_loader = DataLoader(train_data_list, batch_size=1, shuffle=True)
-test_loader = DataLoader(test_data_list, batch_size=1, shuffle=True)
-
+train_loader = DataLoader(train_data_list, batch_size=1, shuffle=True)   #train_data_list
+validation_loader = DataLoader(validation_data_list, batch_size=1, shuffle=True)   #train_data_list
+test_loader = DataLoader(test_data_list, batch_size=1, shuffle=True)    #test_data_list
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
