22de0c13dab3e4f56cab2b68007dcde38fae4b4f
CullenDs <40348774+KingdomKJ@users.noreply.github.com>
2025-12-05 23:07:21 +0800

    Update sciQ_llama2.py

--- Training scripts/sciQ_llama2.py
+++ Training scripts/sciQ_llama2.py
@@ -34,25 +34,17 @@ class GNNModel(torch.nn.Module):
         x = global_mean_pool(x, batch)  # 
         x = self.classifier(x)
         return x
-
 train_file_name = f'sciQ/llama/{args.type}/sciQ_llama_train_data_list.pkl'
+validation_file_name = f'sciQ/llama/{args.type}/sciQ_llama_validation_data_list.pkl'
 test_file_name = f'sciQ/llama/{args.type}/sciQ_llama_test_data_list.pkl'
-
-with open(train_file_name, 'rb') as file1:
-
-    train_data = pickle.load(file1)   
-
-
-with open(test_file_name, 'rb') as file2:
-    test_data = pickle.load(file2)
 
 
 train_data_list = train_data
+validation_data_list=validation_data
 test_data_list = test_data
-
-train_loader = DataLoader(train_data_list, batch_size=1, shuffle=True)
-test_loader = DataLoader(test_data_list, batch_size=1, shuffle=True)
-
+train_loader = DataLoader(train_data_list, batch_size=1, shuffle=True)   #train_data_list
+validation_loader = DataLoader(validation_data_list, batch_size=1, shuffle=True)   #train_data_list
+test_loader = DataLoader(test_data_list, batch_size=1, shuffle=True)    #test_data_list
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
